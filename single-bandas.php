50b1b2c13b58283d3d9045e8a9f0a729f7821711
Francisco <francisco@eltelon.com>
2015-01-20 21:52:30 -0300

    20012152
    1

---
+++ single-bandas.php
@@ -0,0 +1,30 @@
+<?php include('header.php')?>
+<div class="header-bandas" style="background: url(https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/1425540_599550596759274_1550217697_n.jpg?oh=92927c1108f449863c927892595661cf&oe=55312136&__gda__=1433000914_70e0782b2b6389c51c9c8607067e4bac) no-repeat center center ; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover;	background-size: cover;">
+	<div class="contenido">
+		<div class="celda">
+			<h1>INMTK</h1>
+		</div>
+	</div>
+	
+</div>
+<div class="container-fluid">
+		<div class="row contenido-single-bandas">
+			<div class="col-md-12">
+				<h4>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual"</h4>
+				<p>
+					"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?
+					<img src="https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/10613120_713458978701768_5870801900250025065_n.jpg?oh=6477b8498116a02d71c3ca212c175a97&oe=556DDBFA&__gda__=1428492944_8fbc1c105b114bea7b81d334e08ea1f4">
+					<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/70370653&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+					
+					 Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise.
+					 
+					 
+					 <a href="https://soundcloud.com/inmontaukmusic/sets/inmontauk-lp" target="_blank">https://soundcloud.com/inmontaukmusic/sets/inmontauk-lp</a>
+					 
+				</p>
+			</div>
+		</div>
+	</div>
+<div class="clearfix"></div>
+
+<?php include('footer.php')?>
